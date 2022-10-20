72e1e074aea6ba5957c8d659bee4035bb0a41caa
mat25 <mateojean25660@gmail.com>
2022-10-20 12:43:17 +0200

    commit

--- php-tableau/TP-1/pourcentage-avancement.php
+++ php-tableau/TP-1/pourcentage-avancement.php
@@ -1,7 +1,9 @@
 <?php
 
 $tableau = array_fill(0,100000,1) ;
-
+$texteRouge = "\033[31m";
+$texteVert = "\033[32m";
+$normal = "\033[0m";
 
 echo "debut traitement";
 echo PHP_EOL;
@@ -9,9 +11,20 @@ echo PHP_EOL;
 $i=0;
 foreach ($tableau as $a) {
     $pourcentage = $i / 1000;
-    echo "En cours : ".round($pourcentage,0)."%"."\r";
+    echo "En cours : ".$texteRouge.round($pourcentage,0)."%".$normal."\r";
     $i+=1;
 }
+
+
+echo $texteVert."100 %".$normal;
+echo PHP_EOL;
+echo "fin traitement";
+echo PHP_EOL;
+echo PHP_EOL;
+echo "100000 valeurs on été traités";
+
+
+
 
 
 
