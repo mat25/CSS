4ff50b11604c22b2d881e1ff11c01755a6b57510
mat25 <mateojean25660@gmail.com>
2022-10-22 11:53:15 +0200

    commit

--- php-tableau/TP-3/emails.php
+++ php-tableau/TP-3/emails.php
@@ -1,8 +1,26 @@
 <?php
-$emails = ["durand.martin@free.fr","dupond.martin@gmail.com","petit.bernard@laposte.net"];
-foreach ($emails as $email) {
-    $nom = substr(strpos($email,"."),strpos($email,"@"));
-    echo $nom;
+$emails = ["durand.martin@free.fr","dupond.petit@free.fr","petit.bernard@laposte.net"];
+$tableauNomAdresseMail = [];
 
+//on recupere le nom est l'addresse mail de la personne pour le mettre dans un tableau associatif
+foreach ($emails as $email) {
+    $premierPoint = strpos($email,".");
+    $arobase = strpos($email,"@");
+    $nomPersonne = substr($email,$premierPoint+1,$arobase-$premierPoint-1);
+    $tableauNomAdresseMail[$nomPersonne] = $email;
+}
+
+foreach ($tableauNomAdresseMail as $nom => $mail) {
+    echo "$nom a $mail comme adresse mail";
+    echo PHP_EOL;
+}
+
+$nomDeDomaineARechercher = readline("Veuillez indiquer un nom de domaine que vous voulez rechercher : ");
+
+foreach ($tableauNomAdresseMail as $nom => $mail) {
+    if ($nomDeDomaineARechercher == substr($mail,strpos($mail,"@")+1,strlen($mail))) {
+        echo $nom;
+        echo PHP_EOL;
+    }
 }
 
