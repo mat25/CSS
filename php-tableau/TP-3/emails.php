f35dcb55cf9758a9da48cd19996421c1684be48e
mat25 <mateojean25660@gmail.com>
2022-10-22 11:10:09 +0200

    commit

--- php-tableau/TP-3/emails.php
+++ php-tableau/TP-3/emails.php
@@ -1,2 +1,8 @@
 <?php
-$emails = 0;
+$emails = ["durand.martin@free.fr","dupond.martin@gmail.com","petit.bernard@laposte.net"];
+foreach ($emails as $email) {
+    $nom = substr(strpos($email,"."),strpos($email,"@"));
+    echo $nom;
+
+}
+
