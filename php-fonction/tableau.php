cbd05f5b27379f29546efa8bd92249e578dd45a8
mat25 <mateojean25660@gmail.com>
2022-11-16 17:00:36 +0100

    commit test

--- php-fonction/tableau.php
+++ php-fonction/tableau.php
@@ -1,3 +1,18 @@
 <?php
 
+$tableau = [12,15,16,55,44];
+$tableau2 = ["tit","gergr","fsqfz"];
 
+//tableau qui affiche les elements du tableau
+
+function afficherTableau(array $tab) : void {
+    foreach ($tab as $nombre) {
+        echo $nombre." ";
+    }
+    echo PHP_EOL;
+}
+
+
+//appel
+afficherTableau($tableau);
+afficherTableau($tableau2);
